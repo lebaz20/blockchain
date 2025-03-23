cc65be4aee3df86e6cd76f44cf797b84a8576f45
Mohamed Labib <mohamed.labib@pixelogicmedia.com>
2025-03-23 04:59:30 +0200

    add pipelining based on sharding

---
+++ custom-consensus/pbft-sharding-pipelining/config.js
@@ -0,0 +1,18 @@
+// Maximum number of transactions that can be present in a block and transaction pool
+const TRANSACTION_THRESHOLD = process.env.TRANSACTION_THRESHOLD ? parseInt(process.env.TRANSACTION_THRESHOLD, 10) : 5;
+
+// total number of nodes in the network
+const NUMBER_OF_NODES = process.env.NUMBER_OF_NODES ? parseInt(process.env.NUMBER_OF_NODES, 10) : 3;
+
+// improve performance by using a subset of nodes in the network
+const ACTIVE_SUBSET_OF_NODES = process.env.ACTIVE_SUBSET_OF_NODES ||  0.5;
+
+// Minimum number of positive votes required for the message/block to be valid
+const MIN_APPROVALS = 2 * (NUMBER_OF_NODES / 3) + 1;
+
+module.exports = {
+  TRANSACTION_THRESHOLD,
+  NUMBER_OF_NODES,
+  ACTIVE_SUBSET_OF_NODES,
+  MIN_APPROVALS
+};
